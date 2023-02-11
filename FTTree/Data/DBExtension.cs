a9cfb3f1d5e5e969ccf86abf562704e5096b2f46
Sergey Troshkov <kamronis@yandex.ru>
2023-02-11 20:18:49 +0700

    viewer and editor

---
+++ FTTree/Data/DBExtension.cs
@@ -0,0 +1,49 @@
+﻿using FactographData;
+using FTTree.Shared;
+
+namespace FTTree.Data
+{
+    public static class DBExtension
+    {
+        private static bool showLabels = true;
+        private static string lang = MainLayout.defaultLanguage;
+        public static string GetOntLabel(this IFDataService db, string input)
+        {
+            return showLabels ? db.ontology.LabelOfOnto(input) : input;
+        }
+
+        public static string? GetLangText(this IFDataService db, TGroup input, string? enumSpecificatior = null) // TODO:add fallback lang
+        {
+            if (enumSpecificatior is not null && showLabels) // for enumerations
+            {
+                var text = ((Texts)input).Values.FirstOrDefault()?.Text;
+                return String.IsNullOrEmpty(text) ? "" : db.ontology.EnumValue(enumSpecificatior, ((Texts)input).Values.FirstOrDefault()?.Text, lang);
+            }
+            if (input == null)
+            {
+                return null;
+            }
+            //if (langText == null)
+            //{
+            //    langText = ((Texts)input).Values.FirstOrDefault(); // 1) Try default user lang
+            //}
+            var langText = ((Texts)input).Values.FirstOrDefault(val => val.Lang == lang || String.IsNullOrEmpty(val.Lang)); // 2) Try default or empty lang
+            if (langText == null)
+            {
+                langText = ((Texts)input).Values.FirstOrDefault(); // 3) First available lang
+            }
+            return langText?.Text;
+        }
+
+        public static string? GetName(this IFDataService db, TTree ttree)
+        {
+            return db.GetLangText(ttree.Groups.FirstOrDefault(gr => gr.Pred == "http://fogid.net/o/name"));
+        }
+
+        public static bool IsObject(this IFDataService db, string pred)
+        {
+            return db.ontology.DescendantsAndSelf(MainLayout.defaultObjectProp).Contains(pred);
+        }
+
+    }
+}
