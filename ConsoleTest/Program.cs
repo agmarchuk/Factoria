cbc3034f847f674dab1a24129dfd9fbe329bc547
agmarchuk <mag@iis.nsk.su>
2023-03-06 10:18:22 +0700

    РАботаю над новым построителем деревьев Rec

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FactographData;
+using ConsoleTest;
+using static System.Net.Mime.MediaTypeNames;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Start ConsoleTest");
+
+// Сделаю генерацию данных "Фототека" средствами RRecord
+int npersons = 100;
+int nfotos = npersons * 4;
+int nreflections = npersons * 8;
+
+IEnumerable<RRecord> persons = Enumerable.Range(0, npersons)
+    .Select(i => new RRecord()
+    {
+        Id = "p" + i,
+        Tp = "http://fogid.net/o/person",
+        Props = new RProperty[]
+        {
+            new RField() { Prop= "http://fogid.net/o/name", Value = "Иван" + i, Lang = "ru"},
+        }
+    });
+IEnumerable<RRecord> fotos = Enumerable.Range(0, nfotos)
+    .Select(i => new RRecord()
+    {
+        Id = "f" + i,
+        Tp = "http://fogid.net/o/photo-doc",
+        Props = new RProperty[]
+        {
+            new RField() { Prop= "http://fogid.net/o/name", Value = "DSP" + i, Lang = "ru"},
+        }
+    });
+Random rnd = new Random();
+IEnumerable<RRecord> reflections = Enumerable.Range(0, nreflections)
+    .Select(i => new RRecord()
+    {
+        Id = "r" + i,
+        Tp = "http://fogid.net/o/reflection",
+        Props = new RProperty[]
+        {
+            new RLink() { Prop= "http://fogid.net/o/reflected",
+                Resource = "p" + rnd.Next(npersons)},
+            new RLink() { Prop= "http://fogid.net/o/in-doc",
+                Resource = "f" + rnd.Next(nfotos)},
+        }
+    });
+
+// Создаю словарь id -> RRecord
+var dic = persons
+    .Concat(fotos)
+    .Concat(reflections)
+    .ToDictionary(r => r.Id);
+
+// Создаю словарь обратных отношений: id -> RRecord[] - множество записей,
+// ссылающихся на данный идентификатор
+var dic_inverse = persons
+    .Concat(fotos)
+    .Concat(reflections)
+    .SelectMany(r =>
+    {
+        var pairs = r.Props
+            .Where(p => p is RLink)
+            .Select(p => new Tuple<string, RRecord>(((RLink)p).Resource, r));
+        return pairs;
+    })
+    .GroupBy(pa => pa.Item1, pa => pa.Item2)
+    .ToDictionary(paa => paa.Key, paa => paa.ToArray())
+    ;
+//foreach (var x in dic_inverse)
+//{
+//    Console.WriteLine(x.Key + " ");
+//    foreach (var r in x.Value) Console.Write(" " + r.Id);
+//    Console.WriteLine();
+//}
+
+// Теперь определим функцию GetRecord c выходом в виде RRecord
+Func<string, RRecord> getRecord = id =>
+{
+    return dic[id];
+};
+
+// Попробуем
+RRecord rec1 = getRecord("p" + 66);
+Console.WriteLine($"{rec1.Id} {rec1.Tp} {rec1.GetName()}");
+
+// Перехожу в проектирование RecBuilder...
+
+// Теперь пробую конструирование значения или шаблона
+Rec rec = new Rec("p2938", "http://fogid.net/o/person",
+    new Tex("http://fogit.net/o/name", 
+        new TextLan("Привет", "ru"),
+        new TextLan("Hello", "en")),
+    new Str("http://fogid.net/o/from-date", "2009-11-12")
+    );
+
+Rec shablon = new Rec(null, "http://fogid.net/o/person",
+  new Tex("http://fogid.net/o/name"),
+  new Inv("http://fogid.net/o/reflected", 
+    new Rec(null, "http://fogid.net/o/reflection", 
+        new Dir("http://fogid.net/o/in-doc",
+            new Rec(null, "http://fogid.net/o/document",
+              new Tex("http://fogid.net/o/name"),
+              new Str("http://fogid.net/o/from-date")),
+            new Rec(null, "http://fogid.net/o/photo-doc",
+              new Tex("http://fogid.net/o/name"),
+              new Str("http://fogid.net/o/from-date"),
+              new Str("http://fogid.net/o/uri"),
+              new Str("http://fogid.net/o/docmetainfo"))))
+  ));
+
+var rbuilder = new RecBuilder(getRecord, null);
+var item = rbuilder.ToRec("p66", shablon, null);
---
+++ ConsoleTest/RecBuilder.cs
@@ -0,0 +1,146 @@
+﻿using FactographData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest
+{
+    public class RecBuilder
+    {
+        private Func<string, RRecord?> getRecord;
+        private Func<string, RRecord[]?> getInverseRecords;
+        private Rec shablon;
+        public RecBuilder(Func<string, RRecord?> getRecord, 
+            Func<string, RRecord[]?> getInverseRecords,
+            Rec shablon)
+        {
+            this.getRecord = getRecord;
+            this.getInverseRecords = getInverseRecords;
+            this.shablon = shablon;
+            // Заполним словари
+            directInShablon = shablon.Props
+                .Select((p, i) => new Tuple<Pro, int>(p, i))
+                .Where(pa => !(pa.Item1 is Inv))
+                .ToDictionary(pa => pa.Item1.Pred, pa => pa.Item2);
+        }
+        private Dictionary<string, int> directInShablon;
+        private Dictionary<string, int> inverseInShablon;
+
+        public Rec? ToRec(string id, string? forbidden)
+        {
+            RRecord? r = getRecord(id);
+            if (r == null) return null;
+            Rec result = new Rec(r.Id, r.Tp);
+            // Следуем шаблону. Подсчитаем количество стрелок
+            int[] nprops = Enumerable.Repeat<int>(0, shablon.Props.Length)
+                .ToArray();
+            // Другой массив говорит к какому свойству шаблона относится i-й элемент
+            int[] nominshab = new int[r.Props.Length];
+            for (int i = 0; i < r.Props.Length; i++)
+            {
+                RProperty p = r.Props[i];
+                int nom = -1;
+                if (p is RField)
+                {
+                    var pair = Enumerable.Range(0, shablon.Props.Length)
+                        .Select(i => new Tuple<int, Pro>(i, shablon.Props[i]))
+                        .FirstOrDefault(pa =>
+                            pa.Item2.Pred != null &&
+                            pa.Item2.Pred == p.Prop &&
+                            (pa.Item2 is Str || pa.Item2 is Tex));
+                    if (pair != null) nom = pair.Item1;
+                }
+                else if (p is RLink)
+                {
+                    var pair = Enumerable.Range(0, shablon.Props.Length)
+                        .Select(i => new Tuple<int, Pro>(i, shablon.Props[i]))
+                        .FirstOrDefault(pa =>
+                            pa.Item2.Pred != null &&
+                            pa.Item2.Pred == p.Prop &&
+                            (pa.Item2 is Dir));
+                    if (pair != null) nom = pair.Item1;
+                }
+                else if (p is RInverseLink)
+                {
+                    var pair = Enumerable.Range(0, shablon.Props.Length)
+                        .Select(i => new Tuple<int, Pro>(i, shablon.Props[i]))
+                        .FirstOrDefault(pa =>
+                            pa.Item2.Pred != null &&
+                            pa.Item2.Pred == p.Prop &&
+                            (pa.Item2 is Inv));
+                    if (pair != null) nom = pair.Item1;
+                }
+                else throw new Exception("sfwefg");
+                if (nom != -1)
+                {
+                    nprops[nom] += 1;
+                    nominshab[i] = nom;
+                }
+            }
+            // Теперь заполним
+            return null;
+        }
+    }
+
+    // ========== Rec классы ==========
+    public class Rec
+    {
+        public string? Id { get; private set; }
+        public string Tp { get; private set; }
+        public Pro[] Props { get; private set; }
+        public Rec(string? id, string tp, params Pro[] props)
+        {
+            this.Id = id;
+            this.Tp = tp;
+            this.Props = props;
+        }
+    }
+    public abstract class Pro
+    {
+        public string Pred { get; internal set; } = "";
+    }
+    public class Tex : Pro
+    {
+        public TextLan[]? Values { get; internal set; }
+        public Tex(string pred, params TextLan[]? values)
+        {
+            this.Pred = pred;
+            this.Values = values;
+        }
+    }
+    public class Str : Pro
+    {
+        public string? Value { get; private set; }
+        public Str(string pred)
+        {
+            this.Pred = pred;
+        }
+        public Str(string pred, string? value)
+        {
+            this.Pred = pred;
+            this.Value = value;
+        }
+    }
+    public class Dir : Pro
+    {
+        public Rec[] Resources { get; private set; }
+        public Dir(string pred, params Rec[] resources) 
+        {
+            Pred = pred;
+            Resources = resources;
+        }
+    }
+    public class Inv : Pro
+    {
+        public Rec[] Sources { get; private set; }
+        public Inv(string pred, params Rec[] sources)
+        {
+            Pred = pred;
+            Sources = sources;
+        }
+    }
+
+}
