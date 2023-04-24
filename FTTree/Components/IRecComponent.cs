1ecf35dd208caecc195f332d7591b7195b342c6e
Sergey Troshkov <kamronis@yandex.ru>
2023-04-24 18:17:04 +0700

    added viewer by Rec

---
+++ FTTree/Components/IRecComponent.cs
@@ -0,0 +1,15 @@
+﻿using FactographData;
+using FactographData.r;
+using Microsoft.AspNetCore.Components;
+
+namespace FTTree.Components
+{
+    public interface IRecComponent
+    {
+        [Parameter]
+        public Rec[] recs { get; set; }
+
+        [Parameter]
+        public string forbidden { get; set; }
+    }
+}
