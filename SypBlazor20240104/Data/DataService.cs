39b72733f1b86c47fdd44685403f1b18d5f0d86a
agmarchuk <mag@iis.nsk.su>
2024-04-02 10:44:56 +0700

    Добавил стабильную версию SypBlazor в виде publish-дистрибутива и исходник этой версии в виде .zip

--- SypBlazor20240104/Data/DataService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SypBlazor.Data
-{
-    public class DataService : Factograph.Data.FDataService
-    {
-        public DataService() : base("wwwroot/", "wwwroot/Ontology_iis-v14.xml", null)
-        {
-        }
-    }
-}
