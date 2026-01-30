610cdae1e37aba4936ea2e7756db9f6b541c08b3
Alexander Marchuk <mag@iis.nsk.su>
2026-01-30 07:46:13 +0700

    Поиск решения проблем запуска

--- OpenA/Program.cs
+++ OpenA/Program.cs
@@ -6,27 +6,27 @@ builder.Services.AddRazorPages();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/Error");
+//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//    app.UseHsts();
+//}
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
 app.MapStaticAssets();
 app.MapRazorPages()
    .WithStaticAssets();
 app.MapGet("~/", () =>
     //Results.Redirect("/view")); //"/view/syp2001-p-marchuk_a"));
-    Results.Redirect("/index/syp2001-p-marchuk_a"));
+    Results.Redirect("~/index/"));
 
-app.MapControllerRoute(name: "default",
-         pattern: "{controller=Home}/{action=Index}/{id?}");
+//app.MapControllerRoute(name: "default",
+//         pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
